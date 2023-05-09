bcb32e3935eb0690f0bb00d3a41b83fd92745bf2
rapozoffx <caique.felipexavier123@gmail.com>
2023-05-09 05:08:55 -0300

    Add Aim Control
    Add script for character always look into the mouse direction.

---
+++ Assets/Scripts/AimControl.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AimControl : MonoBehaviour
+{
+    Vector2 mousePos = new Vector2();
+    public GameObject template;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 origin = Camera.main.transform.position;
+        Vector3 screenPointWorldSpace = Camera.main.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, 5));
+
+        Vector3 direction = Vector3.Normalize(screenPointWorldSpace -  origin);
+
+        RaycastHit hit;
+        bool hittedGround = Physics.Raycast(origin, direction, out hit, 100f);
+
+        if (hittedGround)
+        {
+            Vector3 lookRotation = Quaternion.LookRotation(Vector3.Normalize(hit.point - transform.position)).eulerAngles;
+
+            transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, lookRotation.y, transform.rotation.eulerAngles.z);
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Instantiate(template, transform.position, transform.rotation);
+        }
+    }
+
+    void OnGUI()
+    {
+        Event currentEvent = Event.current;
+
+        // Get the mouse position from Event.
+        // Note that the y position from Event is inverted.
+        mousePos.x = currentEvent.mousePosition.x;
+        mousePos.y = Camera.main.pixelHeight - currentEvent.mousePosition.y;
+
+    }
+}
---
+++ Assets/Scripts/TopDownController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownController : MonoBehaviour
+{
+    public Transform player;
+    void Start()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        //player.Rotate(Vector3.up, Time.deltaTime * 10, Space.World);
+        
+    }
+}
