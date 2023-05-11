a45987ac95384348c2799e76bc08094f14a610a3
rapozoffx <caique.felipexavier123@gmail.com>
2023-05-11 20:44:20 -0300

    Add Enemy

---
+++ Assets/Scripts/EnemyAttack.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float attackCooldown = 2f;
+    public int attackDamage = 10;
+    private float attackTimer;
+    private GameObject player;
+
+    void Start()
+    {
+        attackTimer = attackCooldown;
+    }
+
+    void Update()
+    {
+        attackTimer += Time.deltaTime;
+
+        if (attackTimer >= attackCooldown && player != null)
+        {
+            player.GetComponent<HpComponent>().TakeDamage(attackDamage);
+            Debug.Log(player.GetComponent<HpComponent>().currentHP);
+            attackTimer = 0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject);
+        if (other.CompareTag("Player") && attackTimer >= attackCooldown)
+        {
+            //salva player
+            player = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            //remover o player da variável
+            player = null;
+        }
+    }
+
+}
