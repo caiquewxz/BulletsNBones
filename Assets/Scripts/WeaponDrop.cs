9113833e7dcfe3cca7491d54432e0a1cb43e4a9e
rapozoffx <caique.felipexavier123@gmail.com>
2023-05-18 23:13:11 -0300

    weapon drop chance value fix

--- Assets/Scripts/WeaponDrop.cs
+++ Assets/Scripts/WeaponDrop.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -6,15 +7,20 @@ using UnityEngine;
 public class WeaponDrop : MonoBehaviour
 {
     public GameObject template;
-    public int dropMinChanceValue;
-    public int dropMaxChanceValue;
+    public float dropChanceValue;
 
     void OnDestroy()
     {
-        int dropChance = Random.Range(dropMinChanceValue, dropMaxChanceValue);
-        if (dropChance > (dropMinChanceValue / dropMaxChanceValue))
+        dropChanceValue = dropChanceValue / 100;
+        int dropChance = (int)RandomRange(0f, 1f);
+        if (dropChance > dropChanceValue)
         {
             GameObject newPlayerWeapon = Instantiate(template, transform.position, transform.rotation);
         }
+    }
+
+    private int RandomRange(float v1, float v2)
+    {
+        throw new NotImplementedException();
     }
 }
